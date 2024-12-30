3a24f263112ff2608bec979c7df5f3b5273ea983
Ganesh <ganeshInduri4@gmail.com>
2024-12-30 17:26:55 +0530

    feat: Add preloader with timeout for initial loading screen
    - Implemented a preloader component to display a loading screen.
    - Used useEffect with a 1.2-second timeout to transition from the
      preloader.
      - Added cleanup for the timeout to prevent memory leaks.
    
    Signed-off-by: Ganesh <ganeshInduri4@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './index.css';
 import NavBar from './components/NavBar';
 import Home from './components/Home/Home';
+import PreLoader from './components/PreLoader';
 import {
   BrowserRouter as Router,
   Route,
   Routes
 } from "react-router-dom";
 function App() {
+  const [load, updateLoad] = useState(true);
+
+  useEffect(()=>{
+    const timer = setTimeout(()=>{
+      updateLoad(false)
+    },1200);
+    return ()=>{
+      clearTimeout(timer)
+    }
+  },[]);
   return (
     <Router>
+        <PreLoader load={load}/>
         <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
