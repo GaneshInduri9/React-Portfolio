7d775ef0a5349760d3e59e78a2e6652461671a32
Ganesh Induri <ganeshinduri4@gmail.com>
2025-02-27 05:48:05 +0530

    Update project
    Signed-off-by: Ganesh Induri <ganeshinduri4@gmail.com>

--- src/components/Projects/Project.js
+++ src/components/Projects/Project.js
@@ -1,6 +1,7 @@
 import { Row } from "react-bootstrap";
 import ProjectCard from "./ProjectCard";
 import Leetcode150 from "../../assets/leetcode150.PNG";
+import FreshFynd from "../../assets/freshFynd.png";
 import weather from "../../assets/weatherApp.PNG";
 import { Col, Container } from "react-bootstrap";
 import Particle from "../Particle";
@@ -15,10 +16,16 @@ function Project() {
         <p style={{ color: "white" }}>
           Some projects that I've worked on recently.
         </p>
-        <Row
-          className="gap-5"
-          style={{ justifyContent: "center", paddingBottom: "10px" }}
-        >
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={FreshFynd}
+              title="FreshFynd"
+              description="FreshFynd, an eCommerce platform offering a wide selection of products with convenient filtering options. This website is designed for a smooth and enjoyable shopping experience, with modern web technologies powering the interface."
+              ghLink="https://github.com/GaneshInduri9/E-commerce"
+              demoLink="https://e-commerce-frontend-psi-beige.vercel.app/"
+            />
+          </Col>
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={weather}
@@ -33,7 +40,7 @@ function Project() {
               imgPath={Leetcode150}
               title="Leetcode-150"
               description="LeetCode 150, is a repo that contains intuation behind the problem approach, it also has various approach from brute force to most optimal. It also covers all the edge cases, the repo gets updated every day with new problems."
-              ghLink="https://github.com/GaneshInduri9/React-Weather-App"
+              ghLink="https://github.com/GaneshInduri9/Leetcode-150"
               demoLink={null}
             />
           </Col>
